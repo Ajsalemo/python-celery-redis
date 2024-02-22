e27fe35fa4bc577e7400120652c1b8d32bc57f5c
Anthony Salemo <ajssalemo@gmail.com>
2024-02-22 11:08:59 -0500

    containerized the application and able to connect to a local containerized redis instance in the same network

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
-
+from celery import shared_task
 from config import celery_init_app
+
+from random import randrange
 
 app = Flask(__name__)
 
@@ -15,3 +17,15 @@ app.config.from_mapping(
 )
 
 celery_app = celery_init_app(app)
+
+
+@shared_task(ignore_result=False)
+def add_together(a: int, b: int) -> int:
+    return a + b
+
+@app.get("/task/add")
+def start_add() -> dict[str, object]:
+    a = randrange(10)
+    b = randrange(20)
+    result = add_together.delay(a, b)
+    return {"result_id": result.id}
